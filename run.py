7a7780e93079eb27a8e8dd6ef10720f6d881696c
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-06-08 07:58:48 +0000

    Adding images and sound

--- run.py
+++ run.py
@@ -10,6 +10,9 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
+# Set the clock to control the game's frame rate
+clock = pygame.time.Clock() 
+
 # Set the width and height of the game window
 window_width = 1200
 window_height = 800
@@ -46,3 +49,39 @@ def rotate_head(image, direction):
         return pygame.transform.rotate(image, 180)
     else:
         return image
+
+# Initialize the mixer module
+pygame.mixer.init()
+
+# Load the sound effect for eating
+eating_sound = pygame.mixer.Sound("assets/audio/eating_fx.mp3")
+
+# Load the volume buttons
+vol_on_img = pygame.image.load("assets/graphic/vol_on.png")
+vol_on_img = pygame.transform.scale(vol_on_img, (50, 50))
+vol_off_img = pygame.image.load("assets/graphic/vol_off.png")
+vol_off_img = pygame.transform.scale(vol_off_img, (50, 50))
+
+
+# Flag to control the sound state
+mute = False
+
+# Load the font for displaying text
+font = pygame.font.Font(None, 36)
+bold_font = pygame.font.Font(None, 48)
+
+# Function to display text on the screen
+def display_text(text, font, color, x, y):
+    text_surface = font.render(text, True, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = (x, y)
+    screen.blit(text_surface, text_rect)
+
+# Function to start the game
+def start_game():
+    global snake_pos, snake_direction, score, food_spawned
+    snake_pos = [(window_width // 2, window_height // 2)]
+    snake_direction = "right"
+    score = 0
+    food_spawned = True
+

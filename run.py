519ddea5e0550d85d2f42dc7fd7c8340d5b0079b
hypergeek-dev <dennis.mariegaard.jensen@gmail.com>
2023-06-08 07:42:11 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import pygame
+import random
+
+# Initialize pygame
+pygame.init()
+
+# Set the width and height of the game window
+window_width = 1200
+window_height = 800
+window_size = (window_width, window_height)
+screen = pygame.display.set_mode(window_size)
+pygame.display.set_caption("Advanced Snake Game")
+
